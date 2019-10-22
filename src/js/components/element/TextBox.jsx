4969103aff9c78e4049cf2a0712c33e7db50775e
lanpai <lan@piyo.cafe>
2019-10-21 20:34:19 -0700

    Implemented auto TextBox resize

--- src/js/components/element/TextBox.jsx
+++ src/js/components/element/TextBox.jsx
@@ -6,13 +6,27 @@ import css from '../../../css/element/TextBox.scss';
 class TextBox extends Component {
     constructor() {
         super();
+
+        this.textarea = React.createRef();
+        this.checkResize = this.checkResize.bind(this);
+    }
+
+    checkResize() {
+        setTimeout(() => {
+            this.textarea.current.style.height = 'auto';
+            this.textarea.current.style.height = this.textarea.current.scrollHeight + 'px';
+        }, 0);
     }
 
     render() {
         return (
             <div className='textbox'>
                 <div>
-                    <textarea placeholder={ this.props.placeholder } rows={ 1 } />
+                    <textarea
+                        ref={ this.textarea }
+                        onKeyDown={ this.checkResize }
+                        placeholder={ this.props.placeholder }
+                        rows={ 1 } />
                 </div>
             </div>
         );
